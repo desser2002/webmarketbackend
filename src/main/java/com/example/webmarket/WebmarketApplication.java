70bde7aa998922c2e23c4ab1a59e503194ca2d16
desser2002 <oxxxymironushe@gmail.com>
2024-10-14 14:14:35 +0200

    fisrt setup

---
+++ src/main/java/com/example/webmarket/WebmarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.webmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmarketApplication.class, args);
+	}
+
+}
